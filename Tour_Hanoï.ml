946e91a21366a63d6fd340a8c3a88621ad551780
lauremivag <165423695+lauremivag@users.noreply.github.com>
2024-06-04 16:04:48 -0700

    Create Tour_Hanoï.ml
    la fonction `hanoi_aux` résout le problème des Tours de Hanoï de manière récursive en déplaçant les disques de la tour source à la tour cible en utilisant la tour auxiliaire. La fonction `hanoi` appelle la fonction auxiliaire avec les paramètres nécessaires.

---
+++ Tour_Hanoï.ml
@@ -0,0 +1,18 @@
+let rec hanoi_aux n source target auxiliary =
+  if n = 1 then
+    print_endline (source ^ " -> " ^ target)
+  else begin
+    hanoi_aux (n-1) source auxiliary target;
+    print_endline (source ^ " -> " ^ target);
+    hanoi_aux (n-1) auxiliary target source
+  end
+
+let hanoi n =
+  hanoi_aux n "A" "C" "B"
+
+let () =
+  let start_time = Sys.time () in
+  hanoi 3;  (* Vous pouvez remplacer 3 par le nombre de disques initial souhaité *)
+  let end_time = Sys.time () in
+  let execution_time = end_time -. start_time in
+  Printf.printf "Temps d'exécution : %f secondes\n" execution_time ;;
